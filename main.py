6865f28dadb0c1790c5276201320c0d395ae4d02
elogeek <elodiechristin@gmail.com>
2023-04-01 14:51:56 +0200

    suite (list)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-# Init the variables
-x = 3
-y = 5
-name = "Elodie"
-name_dev = "Elogeek"
-z = x + y
+# list
+liste_nombres = [1, 6, 98, 52, 1045, 3]
+hobbies =["travel", "Japan+++", "walking in the nature", "food"]
 
-print(name)
-print(z)
-print(len(name_dev))
+# classez la liste
+liste_nombres.sort()
+# supprimez le premier élément de la liste
+liste_nombres.pop(0)
+# ajoutez un nombre à la fin de la liste
+liste_nombres.append(5657)
+# récupérez le deuxième élément dans une variable "deuxieme_element"
+hobby = hobbies[1]
+print(hobby)
+
+# 5) affichez la longueur de la liste
+print(len(liste_nombres))
