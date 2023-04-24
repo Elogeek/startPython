64207fd89bdc4b613f161723c29edabcea911db4
elogeek <elodiechristin@gmail.com>
2023-04-24 14:14:31 +0200

    suite python

--- list.py
+++ list.py
@@ -12,4 +12,19 @@ print(notes)
 result = statistics.mean(notes)
 print("La moyenne de l'élève est de {}".format(result))
 
-#
+# Exo sentence generator
+chained_words = input("Entrer une chaine de la forme mot1/mot2/mot3/mot4")
+
+# Turn this string into a list then mix it
+words = chained_words.split("/")
+shuffle(words)
+
+# Get the number of elements
+words_len = len(words)
+
+# If the number of elements in this list is less than 5, display the first two words
+if words_len < 5:
+    print(words[0], words[1])
+# If the number of elements is greater than or equal to 5 show last 3
+else:
+    print(words[words_len - 1], words[words_len - 2], words[words_len - 3])
