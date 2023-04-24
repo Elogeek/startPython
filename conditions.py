1576095b8ac233d27fb2c156c2ca2e7ca7a10edf
elogeek <elodiechristin@gmail.com>
2023-04-24 10:41:29 +0200

    conditions in python

--- conditions.py
+++ conditions.py
@@ -2,11 +2,14 @@ wallet = 3000
 television_price = 1500
 
 # The price of television is less than 1000€
-if television_price > 1000 and television_price <= wallet:
+if 1000 < television_price <= wallet:
     wallet -= television_price
     print("Le prix de la télévision est bien inférieure à 1000€, l'achat est possible")
 else:
     print("Le prix est supérieur à 1000€, l'achat est impossible, vous n'avez que {}€".format(wallet))
 
+# Condition ternaire
+purchase = ("L'achat est possible", "L'achat est impossible") [television_price <= 1000]
+print(purchase)
 # Show wallet after purchase
 print(wallet)
