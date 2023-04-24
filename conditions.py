96fe94f16f212938def026f2840f3cd883e4c641
elogeek <elodiechristin@gmail.com>
2023-04-24 10:36:47 +0200

    conditions in python

--- conditions.py
+++ conditions.py
@@ -1,6 +1,12 @@
 wallet = 3000
-television_price = 900
+television_price = 1500
 
 # The price of television is less than 1000€
-if television_price < 1000:
-    print("Le prix de la télévision est bien inférieure à 1000€")
+if television_price > 1000 and television_price <= wallet:
+    wallet -= television_price
+    print("Le prix de la télévision est bien inférieure à 1000€, l'achat est possible")
+else:
+    print("Le prix est supérieur à 1000€, l'achat est impossible, vous n'avez que {}€".format(wallet))
+
+# Show wallet after purchase
+print(wallet)
