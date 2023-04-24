66f46e2967ed9800c47db63663b07b38b4a6fc0c
elogeek <elodiechristin@gmail.com>
2023-04-24 10:52:46 +0200

    conditions in python

--- conditions.py
+++ conditions.py
@@ -1,3 +1,4 @@
+# Exo buying a television
 wallet = 3000
 television_price = 1500
 
@@ -8,8 +9,21 @@ if 1000 < television_price <= wallet:
 else:
     print("Le prix est supérieur à 1000€, l'achat est impossible, vous n'avez que {}€".format(wallet))
 
-# Condition ternaire
+# Condition ternary
 purchase = ("L'achat est possible", "L'achat est impossible") [television_price <= 1000]
 print(purchase)
 # Show wallet after purchase
 print(wallet)
+
+# Exo password verification system
+password = input("Entrer votre mot de passe, svp")
+password_length = len(password)
+
+# Check if the password is less than 5 characters
+if password_length <= 5:
+    print("Mot de passe trop court!")
+elif password_length > 5 and password_length <= 8:
+    print("Mot de passe moyen!")
+else:
+    print("Mot de passe super!")
+print(password_length)
