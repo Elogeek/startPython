0aa4ab6d258f27fd09fa47e8ec3d1038e35a6e54
elogeek <elodiechristin@gmail.com>
2023-05-11 11:01:09 +0200

    suite python

--- function.py
+++ function.py
@@ -23,9 +23,19 @@ def max_number(a,b):
         return b
 
 # Value a (first number)
-number_a = int(input("Entrer un nombre"))
+number_a = int(input("Entrer un nombre: "))
 # Value b (second number)
-number_b = int(input("Entrer un deuxième nombre"))
+number_b = int(input("Entrer un deuxième nombre: "))
 max_value = max(number_a, number_b)
 # Result
 print("La plus grosse valeur est ",max_value )
+
+# Recursive function
+def add(c):
+    c += 1
+    print(c)
+
+    # Don't forget the condition otherwise it's an infinite loop, the function doesn't stop ;)
+    if c < 5:
+        add(c)
+add(0)
